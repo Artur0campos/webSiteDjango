7ae37f8e9d6e5197f02db97feb6dbe16f5efd938
Artur0campos <arturcamposcamara@gmail.com>
2024-06-06 14:35:55 -0300

    deleting the style of my home

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -17,9 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
-from myApp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home)
+    path("", include('myApp.urls'))
 ]
