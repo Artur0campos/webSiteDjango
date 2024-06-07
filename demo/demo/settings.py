d59ec703ada9e585b68d8d694f74b2c30b9ebb42
Artur0campos <arturcamposcamara@gmail.com>
2024-06-07 14:30:50 -0300

    editando a static

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'styles'
+            BASE_DIR 
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/styles.css '
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
