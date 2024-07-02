08aa14ef2a11336c365d50d8abc5eac157f7a387
Artur0campos <arturcamposcamara@gmail.com>
2024-07-02 17:13:01 -0300

    dividindo home.html para facilitar o job

--- demo/myApp/views.py
+++ demo/myApp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "MyApp/pages/home.html")
